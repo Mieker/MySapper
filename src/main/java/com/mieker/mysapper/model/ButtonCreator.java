5cc352f63091f2f9108d6ac4bc5e2c422c009550
mieker <mieker@wp.pl>
2023-11-25 15:49:05 +0100

    lombok issue fixed; readme created;

--- src/main/java/com/mieker/mysapper/model/ButtonCreator.java
+++ src/main/java/com/mieker/mysapper/model/ButtonCreator.java
@@ -9,13 +9,15 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.GridPane;
 
+import static com.mieker.mysapper.model.Status.*;
+
 public class ButtonCreator {
 
     Styller styller = new Styller();
 
     public MyButton createButton(int x, int y, Board board, GridPane grid) {
         MyButton button = new MyButton();
-        button.setStatus(Status.COVERED);
+        button.setStatus(COVERED);
         button.setX(x);
         button.setY(y);
         button.setMinSize(35, 35);
@@ -23,7 +25,7 @@ public class ButtonCreator {
         button.setStyle("-fx-font-weight: 700");
         button.setHiddenValue(board.getBoard()[y][x]);
         button.setOnAction(e -> {
-            if (button.getStatus().equals(Status.COVERED)) {
+            if (button.getStatus().equals(COVERED)) {
                 button.setClicked(true);
                 button.setStatus(Status.SHOWED);
                 if (button.getHiddenValue() == 9) {
@@ -43,24 +45,27 @@ public class ButtonCreator {
             Image mine = new Image("flag.png");
             ImageView mineView = new ImageView(mine);
             if (event.getButton() == MouseButton.SECONDARY) {
-                if (button.getStatus().equals(Status.COVERED)) {
-                    MySapperApplication.bombCounter --;
-                    MySapperApplication.refreshLabelValue();
-                    button.setStatus(Status.MARKED);
-                    button.setGraphic(mineView);
-                } else if (button.getStatus().equals(Status.MARKED)) {
+                switch (button.getStatus()) {
+                    case COVERED -> {
+                        MySapperApplication.bombCounter--;
+                        MySapperApplication.refreshLabelValue();
+                        button.setStatus(MARKED);
+                        button.setGraphic(mineView);
 
-                    button.setStatus(Status.GUESS);
-                    button.setGraphic(null);
-                    button.setText("?");
-                    button.setStyle("-fx-font-weight: 700; ");
-                } else if (button.getStatus().equals(Status.GUESS)) {
-                    MySapperApplication.bombCounter ++;
-                    MySapperApplication.refreshLabelValue();
-                    button.setStatus(Status.COVERED);
-                    button.setText("");
+                    }
+                    case MARKED -> {
+                        button.setStatus(GUESS);
+                        button.setGraphic(null);
+                        button.setText("?");
+                        button.setStyle("-fx-font-weight: 700; ");
+                    }
+                    case GUESS -> {
+                        MySapperApplication.bombCounter++;
+                        MySapperApplication.refreshLabelValue();
+                        button.setStatus(COVERED);
+                        button.setText("");
+                    }
                 }
-
             }
         });
         return button;
